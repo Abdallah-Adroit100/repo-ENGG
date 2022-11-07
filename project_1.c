0551cfd935ad32e6a93eecb07c37462849b67c54
Abdallah  Al Hussami <abdallahalhussami@Abdallahs-MacBook-Pro.local>
2022-11-07 12:58:22 -0500

    i added the baces of the code

--- project_1.c
+++ project_1.c
@@ -3,12 +3,23 @@
 #include<math.h>
 #include<string.h>
 
-int inputx, inputy, n, t, i, flag;
+int input_x, input_y, n, t, i, flag;
 char mag_1 [100];
-
-
-
+long int e[50], d[50], temp[50], j, m[50], en[50];
+int prime(long int);
+void encryption_key();
+long int cd(long int);
+void encrypt();
+void decrypt();
 
 int main(){
-
+    printf("\n- ENTER the first Prime number:");
+    scanf("%d",&input_x);
+    flag =prime(input_x);
+    if(flag==0){
+        printf("\n----Wrong Input----\n");
+        exit(0);
+    }
+    printf("\n- Enter second Prime Number:");
+    scanf("")
 }
